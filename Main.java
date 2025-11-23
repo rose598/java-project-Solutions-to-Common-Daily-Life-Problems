0eca5b4bcd827039a63256a4963c189c782911f6
rose yau <2357046517@qq.com>
2025-11-23 09:47:04 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public static void main(String[] args){
+    int a=10,b=15;
+    System.out.println("b>a is "+(b>a));
+    Scanner sc=new Scanner(System.in);
+    int x= sc.nextInt();
+    System.out.println(x);
+
+}
